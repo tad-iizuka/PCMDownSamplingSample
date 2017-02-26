e4afb58ae46f7fe17d9058a70228fc47b3d232fd
tad-iizuka <tad.iizuka@gmail.com>
2017-02-26 13:24:01 +0900

    Initial Commit

---
+++ PCMDownSamplingSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PCMDownSamplingSample
+//
+//  Created by Tadashi on 2017/02/26.
+//  Copyright © 2017 T@d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
